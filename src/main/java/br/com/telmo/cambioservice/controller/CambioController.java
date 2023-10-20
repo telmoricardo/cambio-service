2bdbc939f6031dbe98e6877e7068b6daccd0bbed
Telmo Ricardo <robinette13@theavyk.com>
2023-10-20 14:31:57 -0300

    add openapi docs e swagger-ui

--- src/main/java/br/com/telmo/cambioservice/controller/CambioController.java
+++ src/main/java/br/com/telmo/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.telmo.cambioservice.controller;
 
 import br.com.telmo.cambioservice.model.Cambio;
 import br.com.telmo.cambioservice.service.CambioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio endpoint")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -20,7 +23,7 @@ public class CambioController {
     @Autowired
     private CambioService service;
 
-
+    @Operation(summary = "Find a specific cambio by your amount, from and to")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
             @PathVariable("amount") BigDecimal amount,
